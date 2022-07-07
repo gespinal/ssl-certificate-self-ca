71fd1f52a327fe117f4e870e9e744226a691a597
gespinal <gregorioespinal@gmail.com>
2022-07-07 17:02:04 -0400

    Updating certs path

--- README.md
+++ README.md
@@ -2,18 +2,20 @@
 
 ### Notes:
 
-This script is designed to work on Ubuntu (works on WSL2 perfectly), but can be adapted to work under any linux distribution.
+This script is designed to work on Ubuntu (works on WSL2 perfectly) and macOS, and can be adapted to work under any linux distribution.
 
 ### How to run this:
 
-If you want a local domain, let's say `example.com`, simply run the script.
+If you want a local domain, let's say **example.com**, simply run the script.
 
 `./script.sh example.com` 
 
 This will generate your certificate and key files under the `./certs` directory in the following format...
 
-CERT: `example.com.crt`
+Authority: **example.com-CA.pem**
 
-KEY: `example.com.key` 
+Certificate: **example.com-CERT.pem**
 
-The script will also add it to your OS certificate list, including Windows if under WSL2.
+Key: **example.com.key**
+
+The script will also add it to your OS certificate list. This includes Ubuntu Linux, macOS and Windows if under WSL2.
--- create_certificate.sh
+++ create_certificate.sh
@@ -10,14 +10,12 @@ else
   DOMAIN=$1
 fi
 
-# Vars
-CERTS_DIR=./certs
+CERTS_DIR="${PWD}/certs"
 
 echo "### Check if certificate already exists for ${DOMAIN}..."
 EXISTS=`awk -v cmd='openssl x509 -noout -subject' '/BEGIN/{close(cmd)};{print | cmd}' < /etc/ssl/certs/ca-certificates.crt | grep ${DOMAIN}`
 if [[ ! -z "$EXISTS" ]]; then
   echo "Certificate for ${DOMAIN} already exists on this system."
-  # exit 1;
 fi
 
 echo "### Validating if ${CERTS_DIR} exists..."
@@ -58,51 +56,51 @@ openssl req \
  -x509 \
  -sha256 \
  -days 365 \
- -keyout ${CERTS_DIR}/${DOMAIN}.key \
- -out ${CERTS_DIR}/${DOMAIN}-CA.pem \
+ -keyout "${CERTS_DIR}/${DOMAIN}.key" \
+ -out "${CERTS_DIR}/${DOMAIN}-CA.pem" \
  -subj "/C=DR/ST=SD/L=DR/O=${DOMAIN%.*}/OU=${DOMAIN%.*}/CN=*.${DOMAIN}/emailAddress=admin@${DOMAIN}"
 
 echo "### Create csr for ${DOMAIN}..."
 openssl req -new \
  -newkey rsa:2048 \
- -key ${CERTS_DIR}/${DOMAIN}.key \
+ -key "${CERTS_DIR}/${DOMAIN}.key" \
  -nodes \
- -out ${CERTS_DIR}/${DOMAIN}.csr \
+ -out "${CERTS_DIR}/${DOMAIN}.csr" \
  -extensions v3_req \
- -config ${CERTS_DIR}/${DOMAIN}-csr.conf
+ -config "${CERTS_DIR}/${DOMAIN}-csr.conf"
  
  echo "### Sign the csr with our ca's cert..."
 openssl x509 -req \
- -in ${CERTS_DIR}/${DOMAIN}.csr \
- -CA ${CERTS_DIR}/${DOMAIN}-CA.pem \
- -CAkey ${CERTS_DIR}/${DOMAIN}.key \
+ -in "${CERTS_DIR}/${DOMAIN}.csr" \
+ -CA "${CERTS_DIR}/${DOMAIN}-CA.pem" \
+ -CAkey "${CERTS_DIR}/${DOMAIN}.key" \
  -CAcreateserial \
- -out ${CERTS_DIR}/${DOMAIN}-CERT.pem \
+ -out "${CERTS_DIR}/${DOMAIN}-CERT.pem" \
  -days 365 \
  -sha256 \
- -extfile ${CERTS_DIR}/${DOMAIN}-csr.conf \
+ -extfile "${CERTS_DIR}/${DOMAIN}-csr.conf" \
  -extensions v3_req
  
 echo "### Get ${DOMAIN}.crt data..."
-openssl x509 -text -in ${CERTS_DIR}/${DOMAIN}.CERT.pem
+openssl x509 -text -in "${CERTS_DIR}/${DOMAIN}.CERT.pem"
  
 echo "### Re-check ${DOMAIN}.crt data..."
-openssl req -in ${CERTS_DIR}/${DOMAIN}.csr -noout -text
+openssl req -in "${CERTS_DIR}/${DOMAIN}.csr" -noout -text
 
 echo "### Create ${DOMAIN} fullchain..."
-cat ${CERTS_DIR}/${DOMAIN}-CERT.pem > ${CERTS_DIR}/${DOMAIN}-FULLCHAIN.pem
-cat ${CERTS_DIR}/${DOMAIN}-CA.pem >> ${CERTS_DIR}/${DOMAIN}-FULLCHAIN.pem
+cat "${CERTS_DIR}/${DOMAIN}-CERT.pem" > "${CERTS_DIR}/${DOMAIN}-FULLCHAIN.pem"
+cat "${CERTS_DIR}/${DOMAIN}-CA.pem" >> "${CERTS_DIR}/${DOMAIN}-FULLCHAIN.pem"
 
 echo "### Create ${DOMAIN} key-server chain..."
-cat ${CERTS_DIR}/${DOMAIN}.key > ${CERTS_DIR}/${DOMAIN}-KEYCHAIN.pem
-cat ${CERTS_DIR}/${DOMAIN}-CERT.pem >> ${CERTS_DIR}/${DOMAIN}-KEYCHAIN.pem
+cat "${CERTS_DIR}/${DOMAIN}.key" > "${CERTS_DIR}/${DOMAIN}-KEYCHAIN.pem"
+cat "${CERTS_DIR}/${DOMAIN}-CERT.pem" >> "${CERTS_DIR}/${DOMAIN}-KEYCHAIN.pem"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "### Copy ${DOMAIN} cert file to system dir..."
   sudo cp ${CERTS_DIR}/${DOMAIN}-CA.pem /usr/local/share/ca-certificates/${DOMAIN}.CA.crt
   echo "### If on WSL: Copy ${DOMAIN} cert file into windows store..."
   if [[ $(uname -r) == *"WSL"* ]]; then
-    cp ${CERTS_DIR}/${DOMAIN}-CA.pem ${CERTS_DIR}/${DOMAIN}-FULLCHAIN.pem ${CERTS_DIR}/${DOMAIN}-KEYCHAIN.pem ${CERTS_DIR}/${DOMAIN}.key /mnt/c/Windows/Temp
+    cp "${CERTS_DIR}/${DOMAIN}-CA.pem" "${CERTS_DIR}/${DOMAIN}-FULLCHAIN.pem" "${CERTS_DIR}/${DOMAIN}-KEYCHAIN.pem" "${CERTS_DIR}/${DOMAIN}.key" /mnt/c/Windows/Temp
     powershell.exe Start-Process powershell -Verb RunAs -ArgumentList "Import-Certificate, -FilePath, C:\\Windows\\Temp\\${DOMAIN}-CA.pem, -CertStoreLocation, Cert:\LocalMachine\Root"
   fi
   echo "### Update OS certificates..."
@@ -121,3 +119,6 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "### Validating ${DOMAIN} certificate..."
   security find-certificate -a -e "admin@example.com"
 fi
+
+echo "### If using Firefox (Set system wide certificate validation)..."
+echo "Go to: about:config\nSet: security.enterprise_roots.enabled to true"
